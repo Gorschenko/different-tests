e6baa8a2e1b31d8d60b53f426a54bd08e232516f
Aleksey <aa.gorschenko@yandex.ru>
2024-01-18 13:50:38 +0300

    Factories patterns edited

--- patterns/creational/factoryMethod.js
+++ patterns/creational/factoryMethod.js
@@ -1,76 +1,77 @@
+// First example
 class Bmw {
-    constructor(model, price, maxSpeed) {
-        this.model = model
-        this.price = price
-        this.maxSpeed = maxSpeed
-    }
+  constructor(model, price, maxSpeed) {
+    this.model = model;
+    this.price = price;
+    this.maxSpeed = maxSpeed;
+  }
 }
 
 class BmwFactory {
-    create(type) {
-        if (type === 'X5') {
-            return new Bmw(type, 108000, 300)
-        }
-        if (type === 'X6') {
-            return new Bmw(type, 111000, 320)
-        }
+  create(type) {
+    if (type === "X5") {
+      return new Bmw(type, 108000, 300);
     }
+    if (type === "X6") {
+      return new Bmw(type, 111000, 320);
+    }
+  }
 }
 
-const bmwFactory = new BmwFactory()
+const bmwFactory = new BmwFactory();
 
-const x5 = bmwFactory.create('X5')
-const x6 = bmwFactory.create('X6')
+const x5 = bmwFactory.create("X5");
+const x6 = bmwFactory.create("X6");
 
-// NEW EXAMPLE
+// Second example
 
 class SimpleMembership {
-    constructor(name) {
-        this.name = name
-        this.cost = 50
-    }
+  constructor(name) {
+    this.name = name;
+    this.cost = 50;
+  }
 }
 
 class StandardMembership {
-    constructor(name) {
-        this.name = name
-        this.cost = 150
-    }
+  constructor(name) {
+    this.name = name;
+    this.cost = 150;
+  }
 }
 
 class PremiumMembership {
-    constructor(name) {
-        this.name = name
-        this.cost = 500
-    }
+  constructor(name) {
+    this.name = name;
+    this.cost = 500;
+  }
 }
 
 class MemberFactory {
-    static list = {
-        simple: SimpleMembership,
-        standard: StandardMembership,
-        premium: PremiumMembership,
-    }
+  static list = {
+    simple: SimpleMembership,
+    standard: StandardMembership,
+    premium: PremiumMembership,
+  };
 
-    create(name, type = 'simple') {
-        const Membership = MemberFactory.list[type] || MemberFactory.list.simple
-        const member = new Membership(name)
+  create(name, type = "simple") {
+    const Membership = MemberFactory.list[type] || MemberFactory.list.simple;
+    const member = new Membership(name);
 
-        // можно добавлять любые данные
-        member.type = type
-        member.define = function() {
-            console.log(this.name, this.type, this.cost)
-        }
+    // можно добавлять любые данные
+    member.type = type;
+    member.define = function () {
+      console.log(this.name, this.type, this.cost);
+    };
 
-        return member
-    }
+    return member;
+  }
 }
 
-const memberFactory = new MemberFactory()
+const memberFactory = new MemberFactory();
 
 const members = [
-    memberFactory.create('Member 1', 'simple'),
-    memberFactory.create('Member 2', 'premium'),
-]
+  memberFactory.create("Member 1", "simple"),
+  memberFactory.create("Member 2", "premium"),
+];
 
-members.forEach(m => m.define())
+members.forEach((m) => m.define());
