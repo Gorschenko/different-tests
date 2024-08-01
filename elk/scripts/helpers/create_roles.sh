2c620298f6866ca22a35b3319b513d4b7733222b
Alexey G. <aa.gorschenkO@yandex.ru>
2024-08-01 20:39:42 +0300

    [mod] - elk data edited

--- elk/scripts/helpers/create_roles.sh
+++ elk/scripts/helpers/create_roles.sh
@@ -1,11 +1,11 @@
-set_password() {
+create_logstash_writer_role() {
   local user=$1
   local password=$2
-  local url="${ELASTICSEARCH_URL}/_security/user/${user}/_password"
+  local url="${ELASTICSEARCH_URL}/_security/role/logstash_writer"
   local content_type="Content-Type: application/json"
   local request_body="{\"password\":\"${password}\"}"
 
-  echo "Set password for user ${user} is started"
+  echo "Create logstash_writer is started"
 
   curl -X POST "${url}" -H "${content_type}" -u "${ELASTICSEARCH_SYSTEM_USER}:${ELASTICSEARCH_SYSTEM_PASSWORD}" -d "${request_body}"
 }
--- elk/scripts/start.sh
+++ elk/scripts/start.sh
@@ -1,5 +1,7 @@
 source "./envs/.elk.env"
 source "./elk/scripts/helpers/set_password.sh"
 
-set_password kibana_system ${KIBANA_SYSTEM_PASSWORD}
-set_password logstash_system ${LOGSTASH_SYSTEM_PASSWORD}
+set_password ${KIBANA_SYSTEM_USER} ${KIBANA_SYSTEM_PASSWORD}
+set_password ${LOGSTASH_SYSTEM_USER} ${LOGSTASH_SYSTEM_PASSWORD}
+
+$SHELL
