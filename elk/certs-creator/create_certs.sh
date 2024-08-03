3f3852a5220546260da733b09f1053a9db5c5668
Alexey <aa.gorschenko@yandex.ru>
2024-08-03 16:45:28 +0300

    [mod] - elk data edited

--- elk/certs-creator/create_certs.sh
+++ elk/certs-creator/create_certs.sh
@@ -1,13 +1,18 @@
-# Создаем свой Центр сертификации (СА)
+# Создаем доверенный сертификат (СА)
 elasticsearch-certutil ca --out elastic-stack-ca.p12 --pass ""
 
-# Создаем SSL-сертификаты
+# Создаем SSL-сертификат
 elasticsearch-certutil cert \
     --ca elastic-stack-ca.p12 \
     --ca-pass "" \
-    --in certs/instances.yml \
-    --out certs/elasticsearch-certificates.zip \
+    --out certs/elasticsearch-certificates.p12 \
     --pass ""
-
-unzip certs/elasticsearch-certificates.zip -d certs
-rm certs/elasticsearch-certificates.zip
+    # Если указать, то создаcт сертификаты для каждого узла.
+    # Далее можно установить мод, который будет проверять не только сертификаты, но
+    # и адреса узлов, перечисленных в файле
+    # --out elasticsearch-certificates.zip \
+    # --in certs/instances.yml
+    # --pass ""
+# Далее необходимо разархивировать
+# unzip certs/elasticsearch-certificates.zip -d certs
+# rm certs/elasticsearch-certificates.zip 
