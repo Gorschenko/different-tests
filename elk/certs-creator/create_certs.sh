5ad215908c548bfc79c5f1ffbdebc99472567fdc
Alexey <aa.gorschenko@yandex.ru>
2024-08-10 15:19:04 +0300

    [mod] - elk data edited

--- elk/certs-creator/create_certs.sh
+++ elk/certs-creator/create_certs.sh
@@ -1,15 +1,13 @@
 
-# Создаем сертификаты для соединения между экземплярами es
+# Создаем сертификаты для соединения между экземплярами ES по HTTPS
 
 # Создаем доверенный сертификат (СА)
 elasticsearch-certutil ca --out config/certs/elastic-stack-ca.p12 --pass ""
 
 # Создаем SSL-сертификат и ключ
 elasticsearch-certutil cert \
-    --ca config/certs/elastic-stack-ca.p12 \
-    --ca-pass "" \
-    --out config/certs/elasticsearch-certificates.p12 \
-    --pass ""
+    --ca config/certs/elastic-stack-ca.p12 --ca-pass "" \
+    --out config/certs/elasticsearch-certificates.p12 --pass ""
     # Если указать, то создаcт сертификаты и ключи для каждого узла.
     # Далее можно установить мод, который будет проверять не только сертификаты, но
     # и адреса узлов, перечисленных в файле - full
@@ -20,7 +18,7 @@ elasticsearch-certutil cert \
 # unzip certs/elasticsearch-certificates.zip -d certs
 # rm certs/elasticsearch-certificates.zip 
 
-# Создаем сертификаты для клиентского соединения
+# Создаем сертификаты для подключения Kibana, Logstash и ES по HTTPS
 
 # elasticsearch-certutil http <<EOF
 # n
@@ -29,7 +27,7 @@ elasticsearch-certutil cert \
 # password
 # 12M
 # n - generate certs for miltiple nodes
-# kibana - dns for clients
+# es* - dns of nodes
 # ENTER - confirm dns
 # y - confirm dns
 # enter ips (empty field)
@@ -38,3 +36,9 @@ elasticsearch-certutil cert \
 # password
 # name of file
 # EOF
+
+# Создаем сертификаты для соединения между Клиентами и Kibana по HTTPS 
+
+elasticsearch-certutil cert \
+    -ca config/elasticsearch/elastic-stack-ca.p12 --ca-pass "" \
+    --out config/certs/kibana-certificates.p12 --pass ""
