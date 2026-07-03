588c6cb0bc2ae116577f1672c6b75d3744b45dc5
Горщенко Алексей <alexey.gor@safit.pro>
2026-07-03 23:36:13 +0300

    add: interview

---
+++ interview/event_loop/tasks/1.js
@@ -0,0 +1,10 @@
+console.log("Step 1: In global scope");
+
+setTimeout(() => console.log("Step 2: In setTimeout"));
+
+new Promise((resolve) => {
+  console.log("Step 3: In promise constructor");
+  resolve();
+}).then(() => console.log("Step 4: In then"));
+
+setTimeout(() => console.log("Step 5: In another setTimeout"));
---
+++ interview/event_loop/tasks/2.js
@@ -0,0 +1,12 @@
+console.log("Step 1: In global scope");
+
+setTimeout(() => console.log("Step 2: In setTimeout"));
+
+new Promise((resolve) => {
+  console.log("Step 3: In promise constructor");
+  resolve();
+})
+  .then(() => console.log("Step 4: In then"))
+  .then(() => console.log("Step 5: In another then"));
+
+setTimeout(() => console.log("Step 6: In another setTimeout"));
